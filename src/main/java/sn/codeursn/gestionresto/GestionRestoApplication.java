29036ab4e1e5f5c6029b8f8097d572c771c6936e
Laye WANE <laye1wane@gmail.com>
2019-01-07 14:14:41 +0000

    first commit

---
+++ src/main/java/sn/codeursn/gestionresto/GestionRestoApplication.java
@@ -0,0 +1,14 @@
+package sn.codeursn.gestionresto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionRestoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionRestoApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/sn/codeursn/gestionresto/GestionRestoApplicationTests.java
@@ -0,0 +1,17 @@
+package sn.codeursn.gestionresto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GestionRestoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
